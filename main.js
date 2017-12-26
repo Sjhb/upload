9ebef9ec3b837023ebaba3bdac1be0c490a8488a
王欢 <whmanlin@163.com>
2017-12-26 18:12:44 +0800

    完善分发器，增加测试脚本

--- main.js
+++
@@ -1,9 +0,0 @@
-const HTTP = require('http')
-const FS = require('fs')
-const dispatcher = require('./dispatcher')
-
-let server = HTTP.createServer()
-server.on('request', dispatcher)
-server.listen('9001', () => {
-  console.log('upload server runing')
-})
