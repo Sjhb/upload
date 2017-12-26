7f35742a2c8e28a9c7570b9cfc987b3fd5ee3ce0
王欢 <whmanlin@163.com>
2017-12-26 18:25:48 +0800

    更新监听的端口

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const dispatcher = require('./dispatcher')
 
 let server = HTTP.createServer()
 server.on('request', dispatcher)
-server.listen('8009', () => {
-  console.log('upload server runing on port 8009')
+server.listen('80', () => {
+  console.log('upload server runing on port 80')
 })
