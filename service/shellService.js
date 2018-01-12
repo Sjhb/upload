09cb48465aebfbe2af6714ff62190fced7d1901e
王欢 <whmanlin@163.com>
2018-01-12 12:56:13 +0800

    添加克隆和切换分支方法

--- service/shellService.js
+++ service/shellService.js
@@ -1,7 +1,9 @@
 const cp = require('child_process')
 const util = require('util')
+const path = require('path')
 
-const execFile = util.promisify(cp.execFile)
+const shellPath = path.resolve(__dirname, '..', 'shells')
+const exec = util.promisify(cp.exec)
 
 /**
  * 从远程克隆仓库
@@ -10,31 +12,78 @@ const execFile = util.promisify(cp.execFile)
  * @param {String} cwd 仓库克隆的地址cwd
  * @returns 返回执行结果，格式：{result: Boolean, out: String}
  */
-async function cloneRep (payload, cwd) {
-  if (!cwd) {
+function cloneRep (repSshUrl, cwd) {
+  if (!repSshUrl) {
     return {
       result: false,
       out: '需要指定克隆地址'
     }
   }
-  let cloneProcess = null
-  let clone = execFile('../shells/createrep.sh', {})
-  let res = null
-  try {
-    res = await clone
-    process.kill(cloneProcess.pid)
-    return {
-      result: true,
-      out: res
-    }
-  } catch (err) {
+  if (!cwd) {
     return {
       result: false,
-      out: err
+      out: '需要指定本地仓库地址'
     }
   }
+  const cloneArgs = ['git', 'clone', repSshUrl]
+  const option = {
+    cwd
+  }
+  return exec(cloneArgs.join(' '), option)
+}
+
+/**
+ * 检出分支
+ *
+ * @param {String} repository 仓库绝对地址
+ * @param {String} branch 要检出的分支
+ */
+async function checkoutBranch (repository, branch) {
+}
+
+/**
+ * 设置本地分支跟踪远程分支
+ *
+ * @param {String} repository 仓库绝对地址
+ * @param {String} localBranch 本地分支
+ * @param {String} remoteBranch 要跟踪远程分支
+ */
+async function trackBranch (repository, localBranch, remoteBranch) {
+}
+
+/**
+ * 新建本地分支跟踪远程分支
+ *
+ * @param {String} repository 仓库绝对地址
+ * @param {String} localBranch 本地分支
+ * @param {String} remoteBranch 要跟踪远程分支
+ */
+async function trackNewBranch (repository, remoteBranch, localBranch) {
+  if (!repository) {
+    return {
+      result: false,
+      out: '需要指定仓库地址'
+    }
+  }
+  if (!cwd) {
+    return {
+      result: false,
+      out: '需要指定本地仓库地址'
+    }
+  }
+  const option = {
+    cwd: repository
+  }
+  let checkBranchArgs
+  if (localBranch) {
+    checkBranchArgs = ['git', 'checkout', '-b', localBranch,  `origin/${remoteBranch}`]
+  } else {
+    checkBranchArgs = ['git', 'checkout', '--track', `origin/${remoteBranch}`]
+  }
+  return await exec(checkBranchArgs.join(' '), option)
 }
 
 module.exports = {
-  cloneRep
+  cloneRep,
+  trackNewBranch
 }
